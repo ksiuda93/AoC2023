982a346ab7ee0a3135d91b61877a43167113bb2b
Kacper Siuda <kacper.siuda@gh05t.sh>
2023-12-02 14:00:05 +0100

    init Day02

---
+++ src/Day02.kt
@@ -0,0 +1,21 @@
+object Day02 {
+    fun part1(input: List<String>): Unit {
+        return
+    }
+
+    fun part2(input: List<String>): Unit {
+        return
+    }
+}
+
+
+
+
+
+
+
+fun main() {
+    val input = readInput("Day02t")
+    println("Part 1: ${Day02.part1(input)}")
+    println("Part 2: ${Day02.part2(input)}")
+}
